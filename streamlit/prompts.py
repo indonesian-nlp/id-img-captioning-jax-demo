b11d44eb691de799f6c02bc5152702df046a361f
Galuh Sahid <galuh.tunggadewi@gmail.com>
2021-07-10 20:34:54 +0700

    Add English translation

--- streamlit/prompts.py
+++ streamlit/prompts.py
@@ -1,14 +1,14 @@
 PROMPT_LIST = {
-    "Resep masakan": [
+    "Resep masakan (recipe)": [
         "Berikut adalah cara memasak sate ayam: ",
         "Langkah-langkah membuat kastengel: ",
         "Berikut adalah bahan-bahan membuat nastar: "
     ],
-    "Puisi": [
+    "Puisi (poetry)": [
         "Aku ingin jadi merpati\nTerbat di langit yang damai\nBernyanyi-nyanyi tentang masa depan\n",
         "Terdiam aku satu persatu dengan tatapan binar\nSenyawa merasuk dalam sukma membuat lara\nKefanaan membentuk kelemahan"
     ],
-    "Cerpen": [
+    "Cerpen (short story)": [
         "Putri memakai sepatunya dengan malas. Kalau bisa, selama seminggu ini ia bolos sekolah saja. Namun, Bunda pasti akan marah. Ulangan tengah semester telah selesai. Minggu ini, di sekolah sedang beriangsung pekan olahraga.",
         "\"Wah, hari ini cerah sekali ya,\" ucap Budi ketika ia keluar rumah."
     ]
